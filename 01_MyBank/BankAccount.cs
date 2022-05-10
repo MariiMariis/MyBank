8be0143177a74116ff504e044a58dcea1aba20c7
MariiMariis <mariana.motta94@gmail.com>
2022-05-10 11:18:21 -0300

    Add project files.

---
+++ 01_MyBank/BankAccount.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _01_MyBank
+{
+    public class BankAccount
+    {
+        public string Number { get;}
+        public string Owner { get; set;}
+        public decimal Balance 
+        { 
+            get 
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance = balance + item.Amount;
+                }
+                return balance;
+            } 
+        }
+
+        private static int accountNumberSeed = 123456789;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if(amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "O depósito deve ser possuir valor positivo");
+            }
+            
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+
+        }
+           
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "O saque deve ser possuir valor positivo");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Não há fundos suficientes para este saque.");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+    }
+}
+
+
---
+++ 01_MyBank/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _01_MyBank
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Joseph", 1000);
+            Console.WriteLine($"A conta {account.Number} foi criada para {account.Owner} com saldo de R$ {account.Balance}");
+
+
+            account.MakeWithdrawal(190, DateTime.Now, "Cobertor");
+            Console.WriteLine(account.Balance);
+        }
+    }
+}
